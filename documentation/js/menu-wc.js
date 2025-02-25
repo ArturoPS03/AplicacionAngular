ac3f26ea34edfe959aa748ce96145c6d20329538
ArturoPS03 <pliegosancheza@gmail.com>
2025-02-25 13:21:37 +0100

    Compodoc actualizado

--- documentation/js/menu-wc.js
+++ documentation/js/menu-wc.js
@@ -46,6 +46,130 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                     </ul>
                 </li>
                     <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/Album2Component.html" data-type="entity-link" >Album2Component</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Album3Component.html" data-type="entity-link" >Album3Component</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AlbumComponent.html" data-type="entity-link" >AlbumComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/AppComponent.html" data-type="entity-link" >AppComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CarouselComponent.html" data-type="entity-link" >CarouselComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CreateAccountComponent.html" data-type="entity-link" >CreateAccountComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/EquipmentComponent.html" data-type="entity-link" >EquipmentComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/EventsComponent.html" data-type="entity-link" >EventsComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FilmComponent.html" data-type="entity-link" >FilmComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FooterComponent.html" data-type="entity-link" >FooterComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/FormComponent.html" data-type="entity-link" >FormComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeaderComponent.html" data-type="entity-link" >HeaderComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HeroComponent.html" data-type="entity-link" >HeroComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HomeComponent.html" data-type="entity-link" >HomeComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/JumbotronComponent.html" data-type="entity-link" >JumbotronComponent</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/MonstersComponent.html" data-type="entity-link" >MonstersComponent</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#injectables-links"' :
+                                'data-bs-target="#xs-injectables-links"' }>
+                                <span class="icon ion-md-arrow-round-down"></span>
+                                <span>Injectables</span>
+                                <span class="icon ion-ios-arrow-down"></span>
+                            </div>
+                            <ul class="links collapse " ${ isNormalMode ? 'id="injectables-links"' : 'id="xs-injectables-links"' }>
+                                <li class="link">
+                                    <a href="injectables/ArmorService.html" data-type="entity-link" >ArmorService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/EventsService.html" data-type="entity-link" >EventsService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/MonstersService.html" data-type="entity-link" >MonstersService</a>
+                                </li>
+                                <li class="link">
+                                    <a href="injectables/WeaponService.html" data-type="entity-link" >WeaponService</a>
+                                </li>
+                            </ul>
+                        </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#interfaces-links"' :
+                            'data-bs-target="#xs-interfaces-links"' }>
+                            <span class="icon ion-md-information-circle-outline"></span>
+                            <span>Interfaces</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
+                            <li class="link">
+                                <a href="interfaces/Armor.html" data-type="entity-link" >Armor</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Attack.html" data-type="entity-link" >Attack</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Defense.html" data-type="entity-link" >Defense</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Event.html" data-type="entity-link" >Event</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Monster.html" data-type="entity-link" >Monster</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Resistances.html" data-type="entity-link" >Resistances</a>
+                            </li>
+                            <li class="link">
+                                <a href="interfaces/Weapon.html" data-type="entity-link" >Weapon</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
                         <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
                     </li>
                     <li class="divider"></li>
